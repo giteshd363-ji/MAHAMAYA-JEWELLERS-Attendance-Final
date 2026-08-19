5b1ddc8bb75824033607168f12ab1d4b00b5f8d1
giteshd363-ji <giteshd363@gmail.com>
2026-08-19 23:10:46 +0530

    Update MainActivity.kt

--- app/src/main/java/com/mahamayajewellers/attendance/MainActivity.kt
+++ app/src/main/java/com/mahamayajewellers/attendance/MainActivity.kt
@@ -1,1 +1,19 @@
+package com.mahamayajewellers.attendance
 
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val textView = TextView(this)
+        textView.text = "MAHAMAYA JEWELLERS\nAttendance Sheet"
+        textView.textSize = 24f
+        textView.setPadding(32, 80, 32, 32)
+
+        setContentView(textView)
+    }
+}
